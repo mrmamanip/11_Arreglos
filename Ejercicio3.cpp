2be531675f01df40bd95c5984366490618e372b7
mrmamanip <mamanipumamathiasrey@gmail.com>
2025-06-11 23:56:28 -0500

    Ejercicio 3: Intercambio de elementos de un arreglo. - Actualización: Guía para el usuario.

--- Ejercicio3.cpp
+++ Ejercicio3.cpp
@@ -2,17 +2,20 @@
 using namespace std;
 int main(){
     int n, A[20], aux;
+    cout << "Inserta el numero de elementos del arreglo: ";
     cin >> n;
     for ( int i = 0 ; i < n ; i++ ){
+    	cout << "Introduzca el elemtno " << i << " : ";
         cin >> A[i];
     }
-    for ( int i = 0 ; i < (n/2) ; i++ ){
+    for ( int i = 0 ; i < n/2 ; i++ ){
         aux = A[i];
-        A[i] = A[n - i + 1];
-        A[n - i + 1] = aux;
+        A[i] = A[n - i - 1];
+        A[n - i - 1] = aux;
     }
+    cout << "Serie de elementos intercalados de lugar:" << endl;
     for ( int i = 0 ; i < n ; i++ ){
-        cout << A[i] << endl;
+        cout << A[i] << " ";
     }
     return 0;
 }
