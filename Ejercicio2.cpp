f50e127753e067bea5c5f7ebe715282b210ef157
mrmamanip <mamanipumamathiasrey@gmail.com>
2025-06-11 23:46:08 -0500

    Ejercicio 2: Números mayores al promedio. - Actualización: Guía para el usuario.

--- Ejercicio2.cpp
+++ Ejercicio2.cpp
@@ -2,16 +2,19 @@
 using namespace std;
 int main(){
     int n, s, prom, A[20];
+    cout << "Inserte el numero de elementos del arreglo: "; 
     cin >> n;
     s = 0;
-    for ( int i = 1 ; i < n ; i++ ) {
-        cin >> A[i];
+    for ( int i = 0 ; i < n ; i++ ) {
+        cout << "Introduzca el elemento " << i << " : ";
+		cin >> A[i];
         s = A[i] + s;
     }
     prom = s / n;
-    for ( int i = 1 ; i < n ; i++ ){
+    cout << "Los elementos que exeden al promedio son: ";
+    for ( int i = 0 ; i < n ; i++ ){
         if ( A[i] > prom ){
-            cout << A[i] << endl;
+            cout << A[i] << " ";
         }
     }
     return 0;
