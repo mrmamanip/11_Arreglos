de9deae1e889b8f461c7863dc30cbe945b0222b2
mrmamanip <mamanipumamathiasrey@gmail.com>
2025-06-11 23:26:00 -0500

    Ejercicio 1: Mayor elemento de un arreglo. - Actualización: Guía para el usuario.

--- Ejercicio1.cpp
+++ Ejercicio1.cpp
@@ -2,9 +2,11 @@
 using namespace std;
 int main(){
     int n, max, A[20];
+    cout << "Ingrese el numero de elementos del arreglo: ";
     cin >> n;
     for ( int i = 0 ; i < n ; i++) {
-        cin >> A[i];
+        cout << "Introduzca el elemento " << i << " : ";
+		cin >> A[i];
     }
     max = A[0];
     for ( int i = 1 ; i < n ; i++ ) {
@@ -12,6 +14,7 @@ int main(){
             max = A[i];
         }
     }
+    cout << "El numero maximo introducido es: ";
     cout << max;
     return 0;
 }
